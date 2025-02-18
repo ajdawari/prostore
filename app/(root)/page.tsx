9a729ef99a8554414c78c3a444bcd54136fa1d91
Ahmad Javed Dawari <javed.dawari@gmail.com>
2025-02-17 23:04:44 -0700

    Sample Products & Product List

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,15 @@
-export const metadata = {
-  title: "Home",
-};
+import ProductList from "@/components/shared/product/product-list";
+import sampleData from "@/db/sample-data";
 
 const HomePage = () => {
-  return <div>MKB</div>;
+  return (
+    <div>
+      <ProductList
+        data={sampleData.products}
+        title="Newest Arrivals"
+        limit={4}
+      />
+    </div>
+  );
 };
 export default HomePage;
